fd7a717fab7bf5fce9b627ea44a9d3d775499123
Wagner Sequineli <wagnersequineli_@hotmail.com>
2025-05-27 16:07:35 -0300

    Atualização dos estudos

---
+++ src/ControleFluxo.java
@@ -0,0 +1,5 @@
+public class ControleFluxo {
+    public static void main(String[] args) throws Exception {
+
+    }
+}
---
+++ src/SistemaMedida.java
@@ -0,0 +1,18 @@
+public class SistemaMedida {
+    public static void main(String[] args) {
+        String sigla = "P";
+
+        if (sigla == "P")
+            System.out.println("PEQUENO");
+
+        else if (sigla == "M")
+            System.out.println("MÉDIO");
+
+        else if (sigla == "G")
+            System.out.println("GRANDE");
+
+        else
+            System.out.println("INDEFINIDO");
+    }
+
+}
